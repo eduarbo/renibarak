2b0dbe3ad4dbd25aa7d5e90ead911a43c11bfca3
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-09-20 19:41:37 -0500

    add basic helpers: toKey() & fromKey()

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,160 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { arrows, letters, numbers } from './keyCodes';
+
+type Partial<T> = {
+    [P in keyof T]?: T[P];
+};
+
+type ToModifier = 'caps_lock' | 'left_command' | 'left_control' | 'left_option' | 'left_shift' | 'right_command' | 'right_control' | 'right_option' | 'right_shift' | 'fn';
+type FromModifier = ToModifier | 'command' | 'control' | 'option' | 'shift' | 'any';
+
+type KeyCode = { key_code: string };
+type ConsumerKeyCode = { consumer_key_code: string };
+type PointingButton = { pointing_button: string };
+type AnyEvent = 'key_code' | 'consumer_key_code' | 'pointing_button';
+type Any = { any: AnyEvent };
+type Simultaneous = Array<KeyCode | ConsumerKeyCode | PointingButton | Any>;
+
+type InputSource = {
+  language?: string,
+  input_source_id?: string,
+  input_mode_id?: string,
+};
+
+type MouseKey = {
+  x?: number,
+  y?: number,
+  vertical_wheel?: number,
+  horizontal_wheel?: number,
+  speed_multiplier?: number,
+};
+
+type VarType = {
+  name: string,
+  value: string,
+};
+
+type ToOptions = {
+  lazy?: boolean,
+  repeat?: boolean,
+  halt?: boolean,
+  hold_down_milliseconds?: number,
+};
+
+type ToBaseEvent = ToOptions & {
+  modifiers?: ToModifier[],
+};
+
+type ToKeyCode = ToBaseEvent & KeyCode;
+type ToConsumerKeyCode = ToBaseEvent & ConsumerKeyCode;
+type ToPointingButton = ToBaseEvent & PointingButton;
+type ToShellCommand = ToBaseEvent & { shell_command: string };
+type ToSelectInputSource = ToBaseEvent & { select_input_source: InputSource };
+type ToSetVariable = ToBaseEvent & { set_variable: VarType };
+type ToMouseKey = ToBaseEvent & { mouse_key: MouseKey };
+type ToEvent = ToKeyCode | ToConsumerKeyCode | ToPointingButton | ToShellCommand | ToSelectInputSource | ToSetVariable | ToMouseKey;
+
+type FromModifiers = {
+  mandatory?: FromModifier[],
+  optional?: FromModifier[],
+}
+
+type FromBaseEvent = {
+  modifiers?: FromModifiers,
+};
+
+type FromKeyCode = FromBaseEvent & KeyCode;
+type FromConsumerKeyCode = FromBaseEvent & ConsumerKeyCode;
+type FromPointingButton = FromBaseEvent & PointingButton;
+type FromAny = FromBaseEvent & Any;
+type FromEvent = FromKeyCode | FromConsumerKeyCode | FromPointingButton | FromAny;
+type KeyRestriction = 'strict' | 'strict_inverse' | 'insensitive';
+type FromSimultaneous = FromBaseEvent & {
+  simultaneous: Simultaneous
+  simultaneous_options?: {
+    detect_key_down_uninterruptedly?: boolean,
+    key_down_order?: KeyRestriction,
+    key_up_order?: KeyRestriction,
+    key_up_when?: 'any' | 'all',
+    to_after_key_up?: ToEvent[],
+  },
+};
+
+type FrontmostApplicationCondition = {
+  type: 'frontmost_application_if' | 'frontmost_application_unless',
+  bundle_identifiers?: string[],
+  file_paths?: string[],
+  description?: string,
+};
+
+type DeviceCondition = {
+  type: 'device_if' | 'device_unless',
+  identifiers: {
+    vendor_id?: number,
+    product_id?: number,
+    location_id?: number,
+    is_keyboard?: boolean,
+    is_pointing_device?: boolean,
+    description?: string
+  },
+  description?: string,
+}
+
+type KeyboardCondition = {
+  type: 'keyboard_type_if' | 'keyboard_type_unless'
+  keyboard_types: Array<'ansi' | 'iso' | 'jis'>,
+  description?: string,
+}
+
+type InputSourceCondition = {
+  type: 'input_source_if' | 'input_source_unless',
+  input_sources: Array<{
+    language?: string,
+    input_source_id?: string,
+    input_mode_id?: string,
+  }>,
+  description?: string,
+}
+
+type VariableCondition = {
+  type: 'variable_if' | 'variable_unless',
+  name: string,
+  value: string | number,
+  description?: string,
+}
+
+type Condition = FrontmostApplicationCondition | DeviceCondition | KeyboardCondition | InputSourceCondition | VariableCondition;
+
+type Manipulator = {
+  type: 'basic',
+  from: FromEvent,
+  to?: ToEvent[],
+  to_if_alone?: ToEvent[],
+  to_if_held_down?: ToEvent[],
+  to_after_key_up?: ToEvent[],
+  to_delayed_action?: {
+    to_if_invoked?: ToEvent[],
+    to_if_canceled?: ToEvent[],
+  },
+  conditions?: Condition[],
+  parameters?: {
+    'basic.to_if_alone_timeout_milliseconds?': number,
+    'basic.to_if_held_down_threshold_milliseconds?': number,
+    'basic.simultaneous_threshold_milliseconds?': number,
+    'basic.to_delayed_action_delay_milliseconds?': number,
+  },
+  description?: 'string',
+}
+
+export const fromKey = (key: string, mandatory: FromModifier[] = [], optional: FromModifier[] = []): FromKeyCode => ({
+  key_code: key,
+  modifiers: {
+    mandatory,
+    optional,
+  }
+});
+
+export const toKey = (key: string, modifiers: ToModifier[] = [], opts: ToOptions = {}): ToKeyCode => ({
+  key_code: key,
+  modifiers,
+  ...opts,
+});
---
+++ src/keyCodes.ts
@@ -0,0 +1,354 @@
+/* tslint:disable:object-literal-sort-keys */
+
+export const letters = {
+  A: 'a',
+  B: 'b',
+  C: 'c',
+  D: 'd',
+  E: 'e',
+  F: 'f',
+  G: 'g',
+  H: 'h',
+  I: 'i',
+  J: 'j',
+  K: 'k',
+  L: 'l',
+  M: 'm',
+  N: 'n',
+  O: 'o',
+  P: 'p',
+  Q: 'q',
+  R: 'r',
+  S: 's',
+  T: 't',
+  U: 'u',
+  V: 'v',
+  W: 'w',
+  X: 'x',
+  Y: 'y',
+  Z: 'z',
+};
+
+export const modifiers = {
+  CAPS: 'caps_lock',
+  CAP: 'caps_lock',
+  LCTRL: 'left_control',
+  LCTL: 'left_control',
+  LCT: 'left_control',
+  LSHF: 'left_shift',
+  LSHFT: 'left_shift',
+  LALT: 'left_option',
+  LAL: 'left_option',
+  LOPT: 'left_option',
+  LOP: 'left_option',
+  LCMD: 'left_command',
+  RCTL: 'right_control',
+  RSHF: 'right_shift',
+  RSHFT: 'right_shift',
+  RALT: 'right_option',
+  RAL: 'right_option',
+  ROPT: 'right_option',
+  ROP: 'right_option',
+  RCMD: 'right_command',
+  FN: 'fn',
+};
+
+export const numbers = {
+  _1: '1',
+  _2: '2',
+  _3: '3',
+  _4: '4',
+  _5: '5',
+  _6: '6',
+  _7: '7',
+  _8: '8',
+  _9: '9',
+  _0: '0',
+};
+
+export const symbols = {
+  RET: 'return_or_enter',
+  ESC: 'escape',
+  BSPC: 'delete_or_backspace',
+  BSP: 'delete_or_backspace',
+  DEL: 'delete_forward',
+  TAB: 'tab',
+  SPC: 'spacebar',
+  COM: 'comma',
+  DOT: 'period',
+  MIN: 'hyphen',
+  EQL: 'equal_sign',
+  EQ: 'equal_sign',
+  OBRC: 'open_bracket',
+  OBR: 'open_bracket',
+  LBRC: 'open_bracket',
+  LBR : 'open_bracket',
+  CBRC: 'close_bracket',
+  CBR: 'close_bracket',
+  RBRC: 'close_bracket',
+  RBR: 'close_bracket',
+  SLSH: 'slash',
+  SLS: 'slash',
+  BSLS: 'backslash',
+  BSL: 'backslash',
+  SCLN: 'semicolon',
+  SCL: 'semicolon',
+  QOT: 'quote',
+  GRV: 'grave_accent_and_tilde',
+  NUSPND: 'non_us_pound',
+  NUSBLSH: 'non_us_backslash',
+};
+
+export const arrows = {
+  UP: 'up_arrow',
+  DOWN: 'down_arrow',
+  DWN: 'down_arrow',
+  DW: 'down_arrow',
+  LEFT: 'left_arrow',
+  LFT: 'left_arrow',
+  RIGHT: 'right_arrow',
+  RGT: 'right_arrow',
+  PAGEUP: 'page_up',
+  PUP: 'page_up',
+  PAGEDOWN: 'page_down',
+  PDW: 'page_down',
+  HOME: 'home',
+  HOM: 'home',
+  END: 'end',
+};
+
+export const functions = {
+  F1: 'f1',
+  F2: 'f2',
+  F3: 'f3',
+  F4: 'f4',
+  F5: 'f5',
+  F6: 'f6',
+  F7: 'f7',
+  F8: 'f8',
+  F9: 'f9',
+  F10: 'f10',
+  F11: 'f11',
+  F12: 'f12',
+  F13: 'f13',
+  F14: 'f14',
+  F15: 'f15',
+  F16: 'f16',
+  F17: 'f17',
+  F18: 'f18',
+  F19: 'f19',
+  F20: 'f20',
+};
+
+export const functionsNotTo = {
+  F21: 'f21',
+  F22: 'f22',
+  F23: 'f23',
+  F24: 'f24',
+};
+
+export const media = {
+  MUTE: 'mute',
+  VOLUME_DECREMENT: 'volume_decrement',
+  VOLUME_INCREMENT: 'volume_increment',
+};
+
+export const mediaNotFrom = {
+  DISPLAY_BRIGHTNESS_DECREMENT: 'display_brightness_decrement',
+  DISPLAY_BRIGHTNESS_INCREMENT: 'display_brightness_increment',
+  MISSION_CONTROL: 'mission_control',
+  LAUNCHPAD: 'launchpad',
+  DASHBOARD: 'dashboard',
+  ILLUMINATION_DECREMENT: 'illumination_decrement',
+  ILLUMINATION_INCREMENT: 'illumination_increment',
+  REWIND: 'rewind',
+  PLAY_OR_PAUSE: 'play_or_pause',
+  FASTFORWARD: 'fastforward',
+  EJECT: 'eject',
+  APPLE_DISPLAY_BRIGHTNESS_DECREMENT: 'apple_display_brightness_decrement',
+  APPLE_DISPLAY_BRIGHTNESS_INCREMENT: 'apple_display_brightness_increment',
+  APPLE_TOP_CASE_DISPLAY_BRIGHTNESS_DECREMENT: 'apple_top_case_display_brightness_decrement',
+  APPLE_TOP_CASE_DISPLAY_BRIGHTNESS_INCREMENT: 'apple_top_case_display_brightness_increment',
+};
+
+export const keypad = {
+  KEYPAD_NUM_LOCK: 'keypad_num_lock',
+  KEYPAD_SLASH: 'keypad_slash',
+  KEYPAD_ASTERISK: 'keypad_asterisk',
+  KEYPAD_HYPHEN: 'keypad_hyphen',
+  KEYPAD_PLUS: 'keypad_plus',
+  KEYPAD_ENTER: 'keypad_enter',
+  KEYPAD_1: 'keypad_1',
+  KEYPAD_2: 'keypad_2',
+  KEYPAD_3: 'keypad_3',
+  KEYPAD_4: 'keypad_4',
+  KEYPAD_5: 'keypad_5',
+  KEYPAD_6: 'keypad_6',
+  KEYPAD_7: 'keypad_7',
+  KEYPAD_8: 'keypad_8',
+  KEYPAD_9: 'keypad_9',
+  KEYPAD_0: 'keypad_0',
+  KEYPAD_PERIOD: 'keypad_period',
+  KEYPAD_EQUAL_SIGN: 'keypad_equal_sign',
+  KEYPAD_COMMA: 'keypad_comma',
+}
+
+// - category: Virtual Keys
+// - name: vk_none
+//   label: vk_none (disable this key)
+//   not_from: true
+
+// - category: Keys in pc keyboards
+// - name: print_screen
+// - name: scroll_lock
+// - name: pause
+// - name: insert
+// - name: application
+// - name: help
+// - name: power
+// - name: execute
+//   not_to: true
+// - name: menu
+//   not_to: true
+// - name: select
+//   not_to: true
+// - name: stop
+//   not_to: true
+// - name: again
+//   not_to: true
+// - name: undo
+//   not_to: true
+// - name: cut
+//   not_to: true
+// - name: copy
+//   not_to: true
+// - name: paste
+//   not_to: true
+// - name: find
+//   not_to: true
+
+// - category: International keys
+// - name: international1
+// - name: international2
+//   not_to: true
+// - name: international3
+// - name: international4
+//   not_to: true
+// - name: international5
+//   not_to: true
+// - name: international6
+//   not_to: true
+// - name: international7
+//   not_to: true
+// - name: international8
+//   not_to: true
+// - name: international9
+//   not_to: true
+// - name: lang1
+// - name: lang2
+// - name: lang3
+//   not_to: true
+// - name: lang4
+//   not_to: true
+// - name: lang5
+//   not_to: true
+// - name: lang6
+//   not_to: true
+// - name: lang7
+//   not_to: true
+// - name: lang8
+//   not_to: true
+// - name: lang9
+//   not_to: true
+
+// - category: Japanese
+// - name: japanese_eisuu
+//   label: 英数キー
+// - name: japanese_kana
+//   label: かなキー
+// - name: japanese_pc_nfer
+//   label: PCキーボードの無変換キー
+//   not_to: true
+// - name: japanese_pc_xfer
+//   label: PCキーボードの変換キー
+//   not_to: true
+// - name: japanese_pc_katakana
+//   label: PCキーボードのかなキー
+//   not_to: true
+
+// - category: Others
+// - name: keypad_equal_sign_as400
+//   not_to: true
+// - name: locking_caps_lock
+//   not_to: true
+// - name: locking_num_lock
+//   not_to: true
+// - name: locking_scroll_lock
+//   not_to: true
+// - name: alternate_erase
+//   not_to: true
+// - name: sys_req_or_attention
+//   not_to: true
+// - name: cancel
+//   not_to: true
+// - name: clear
+//   not_to: true
+// - name: prior
+//   not_to: true
+// - name: return
+//   label: rarely used return (HID usage 0x9e)
+//   not_to: true
+// - name: separator
+//   not_to: true
+// - name: out
+//   not_to: true
+// - name: oper
+//   not_to: true
+// - name: clear_or_again
+//   not_to: true
+// - name: cr_sel_or_props
+//   not_to: true
+// - name: ex_sel
+//   not_to: true
+// - name: left_alt
+//   label: left_alt (equal toleft_option)
+// - name: left_gui
+//   label: left_gui (equal toleft_command)
+// - name: right_alt
+//   label: right_alt (equal toright_option)
+// - name: right_gui
+//   label: right_gui (equal toright_command)
+// - name: vk_consumer_brightness_down
+//   label: vk_consumer_brightness_down (equal todisplay_brightness_decrement)
+//   not_from: true
+// - name: vk_consumer_brightness_up
+//   label: vk_consumer_brightness_up (equal todisplay_brightness_increment)
+//   not_from: true
+// - name: vk_mission_control
+//   label: vk_mission_control (equal tomission_control)
+//   not_from: true
+// - name: vk_launchpad
+//   label: vk_launchpad (equal tolaunchpad)
+//   not_from: true
+// - name: vk_dashboard
+//   label: vk_dashboard (equal todashboard)
+//   not_from: true
+// - name: vk_consumer_illumination_down
+//   label: vk_consumer_illumination_down (equal toillumination_decrement)
+//   not_from: true
+// - name: vk_consumer_illumination_up
+//   label: vk_consumer_illumination_up (equal toillumination_increment)
+//   not_from: true
+// - name: vk_consumer_previous
+//   label: vk_consumer_previous (equal torewind)
+//   not_from: true
+// - name: vk_consumer_play
+//   label: vk_consumer_play (equal toplay)
+//   not_from: true
+// - name: vk_consumer_next
+//   label: vk_consumer_next (equal tofastforward)
+//   not_from: true
+// - name: volume_down
+//   label: volume_down (equal tovolume_decrement)
+// - name: volume_up
+//   label: volume_up (equal tovolume_increment`)
